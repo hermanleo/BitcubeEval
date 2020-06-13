a892aa85b3546bccbd263eb6f656b81577bcb5f0
hermanleo <hermanlabuschagne98@gmail.com>
2020-06-13 09:49:30 +0200

    Commited section2

---
+++ LoginPageSection2/Welcome.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "Welcome " . $_COOKIE['user'];
+        if ($_COOKIE['remember']=="NO")
+        {
+            setcookie("user", "", time() - 1);
+            setcookie("pass", "", time() - 1);
+            setcookie("remember", "", time() - 1);
+        }
+        ?>
+    </body>
+</html>
